64bda2c81a4684506f444cccf126662e5ab9d391
meskv <krsk1495@gmail.com>
2022-03-13 21:59:33 +0530

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
 //   Collapsible start
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, options);
+  var instances = M.Collapsible.init(elems);
 });
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.collapsible').collapsible();
-});
-// collapsible
-
-
-// back to top
-const htmlElement = document.querySelector("html");
-const scrollToTop = document.querySelector("#scrollToTop");
-
-scrollToTop.addEventListener("click", () => {
-  $(htmlElement).animate({ scrollTop: $(htmlElement).offset().top }, 400);
-});
-
-document.addEventListener("scroll", () => {
-  if (htmlElement.scrollTop > 0) {
-    scrollToTop.style.opacity = 1;
-  } else {
-    scrollToTop.style.opacity = 0;
-  }
-});
-
-
-
-// const htmlElement = document.querySelector("html");
-// const toTopBtn = document.querySelector("#scrollToTop");
-
-// toTopBtn.addEventListener("click", () => {
-//     htmlElement.scrollTop = 0;
-// });
